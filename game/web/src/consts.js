2d01af250506644b97aa8669ea8f0053d65b93e4
BrosLunas <broslunasce@gmail.com>
2024-04-28 10:50:52 +0200

    Initial Commit

---
+++ game/web/src/consts.js
@@ -0,0 +1,43 @@
+export const PROJECTS = [
+  {
+    slug: "01-tinder-swipe",
+    title: "Tinder Swipe",
+    description:
+      "Aprende a crear el efecto de swipe de Tinder con HTML, CSS y JavaScript.",
+    learnings: [
+      "DOM",
+      "Animaciones con gestos",
+      "Eventos de mouse y touch",
+      "Transiciones CSS",
+    ],
+    youtube: 'https://www.youtube.com/watch?v=u01WD_YNENY'
+  },
+  {
+    slug: "02-arkanoid-game",
+    title: "Arkanoid Game",
+    description:
+      "Juego mítico y clásico de Arkanoid para controlar con teclado",
+    learnings: [
+      "Dibujar en Canvas",
+      "Eventos de teclado",
+      "RequestAnimationFrame",
+      "Sprites"
+    ],
+    youtube: 'https://www.youtube.com/watch?v=b6du6MvQmuQ'
+  },
+  {
+    slug: "03-midu-typing-game",
+    title: "Reto de Mecanografía",
+    description: "Pon a prueba tus habilidades de mecanografía con este reto",
+    learnings: [
+      "DOM",
+      "Eventos de teclado",
+      "Manejo de clases",
+      "Optimización selectores"
+    ],
+    theme: {
+      isDark: true
+    },
+    youtube: 'https://www.youtube.com/watch?v=157qVlTelOg&t=135s'
+  }
+]
